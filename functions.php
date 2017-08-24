e402d0b0ecc03db5393e05aafc2ed99457e3a125
Mathieu Sarrasin <contact@iceable.com>
2017-08-24 11:34:49 +0200

    2017-08-24 Enhanced HTML5Shiv enqueuing

--- functions.php
+++ functions.php
@@ -179,12 +179,15 @@ function blackoot_scripts() {
 
 	if ( function_exists( 'get_theme_file_uri' ) ): // WordPress 4.7
 		wp_enqueue_script('blackoot', get_theme_file_uri( '/js/blackoot.min.js' ), array('jquery','hoverIntent'), BLACKOOT_THEME_VERSION );
+		// Loads HTML5 JavaScript file to add support for HTML5 elements for IE < 9.
+    wp_enqueue_script( 'html5shiv', get_theme_file_uri( '/js/html5.js' ), array(), BLACKOOT_THEME_VERSION );
 	else: // Support for WordPress <4.7 (to be removed after 4.9 is released)
 		wp_enqueue_script('blackoot', BLACKOOT_THEME_DIR_URI . '/js/blackoot.min.js', array('jquery','hoverIntent'), BLACKOOT_THEME_VERSION );
+		// Loads HTML5 JavaScript file to add support for HTML5 elements for IE < 9.
+    wp_enqueue_script( 'html5shiv', BLACKOOT_THEME_DIR_URI . '/js/html5.js', array(), BLACKOOT_THEME_VERSION );
 	endif;
 
-	// Loads HTML5 JavaScript file to add support for HTML5 elements for IE < 9.
-  wp_enqueue_script( 'html5shiv', BLACKOOT_THEME_DIR_URI . '/js/html5.js' );
+	// Add conditional for HTML5Shiv to only load for IE < 9
   wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
 	/* Threaded comments support */
